25384b4f94e193cd7291ce22fc89a27bf816d6ba
Lali Bermudez <laliberbello@gmail.com>
2018-11-29 15:50:56 -0500

    Add nav-bar to contactthanks.php

---
+++ contactthanks.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+
+<head>
+	<title>Contact Form Thanks</title>
+  <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="css/style.css" />
+</head>
+
+<body>
+
+  <nav class="navbar fixed-top navbar-expand-md navbar-dark" style="background-color: #f07f83;">
+    <div class="navbar-brand d-flex">
+      <div class="logo">LB</div>
+      <div class="d-flex-wrap text-center logo-name">
+        <span class="d-block logo-title">LALI BERMUDEZ</span>
+        <span class="d-block logo-subtitle">WEB DEVELOPER</span>
+      </div>
+    </div>
+
+
+
+
+
+  </nav>
+	<div>
+
+		<img src="images/title.gif" alt="A Nice & Simple Contact Form" />
+		<p>By <a href="http://css-tricks.com">CSS-Tricks</a></p>
+
+
+		<br /><br />
+
+
+		<h1>Your message has been sent!</h1><br />
+
+		<p><a href="index.html">Back to Contact Form</a></p>
+
+	</div>
+
+</body>
+
+</html>
+<!-- contactthanks from https://css-tricks.com/nice-and-simple-contact-form/ -->
