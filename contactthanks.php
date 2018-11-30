5336c80f24cde628995275fb21ecb826777c60a0
Lali Bermudez <laliberbello@gmail.com>
2018-11-29 21:40:17 -0500

    Style contactthanks as a card, fix intro size

--- contactthanks.php
+++ contactthanks.php
@@ -3,33 +3,24 @@
 <head>
 	<title>Contact Form Thanks</title>
   <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-	<link rel="stylesheet" type="text/css" href="css/style.css" />
+  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="css/styles.css" />
 </head>
 
 <body>
-
-  <nav class="navbar navbar-expand-md navbar-dark" style="background-color: #f07f83;">
-    <div class="navbar-brand d-flex">
-      <div class="logo">LB</div>
-      <div class="d-flex-wrap text-center logo-name">
-        <span class="d-block logo-title">LALI BERMUDEZ</span>
-        <span class="d-block logo-subtitle">WEB DEVELOPER</span>
+  <div class="container">
+    <div class="card">
+      <div class="card-header">
+          Success!
+      </div>
+      <div class="card-body">
+        <h5 class="card-title">Your message has been sent.</h5>
+        <p class="card-text">Thank you for contacting me, I'll get back to you soon.</p>
+        <a href="index.html" class="btn btn-primary">Back to the main page</a>
+        </div>
       </div>
     </div>
-  </nav>
-
-	<div>
-    <h1 class="sent-message">Your message has been sent!</h1>
-
-		<br /><br />
-
-
-		<h2 class="thanks-message">Thank you for contacting me, I'll get back to you soon.</h2><br />
-
-		<p class="back-link"><a href="index.html">Back to the main page</a></p>
-
-	</div>
-
+  </div>
 </body>
 
 </html>
